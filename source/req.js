17eccbb79a37db973cd690db10e6dcc2abf7e845
Gilles Cochez <git@csurl.eu>
2012-05-11 22:51:47 +0100

    Optimized overloaded function for minifier

--- source/req.js
+++ source/req.js
@@ -1,210 +1,212 @@
 /*! github.com/gillescochez/req.js */
 var req = window.req || (function(doc) {
 
-	// private
-	var 
-	
-		// settings
-		settings = {
-			path: './modules/',
-			order: true
-		},
-		
-		// stack to process
-		stack = {},
-		
-		// track loaded and loading resources
-		loaded = {},
-		loading = {},
-		
-		// store objects created from "module" declaration
-		objects = {},
-		
-		// settings setter/getter
-		config = function(name, value) {
-			if (!settings[name]) throw name + ' is not a configuration parameter';
-			if (value) settings[name] = value;
-			else return settings[name];
-		},
-		
-		// helper function to iterate over arrays (if key true return the key as first argument and balue second)
-		each = function(arr, fn, key) {
-			var len = arr.length, i = 0;
-			for (; i < len; i++) fn.apply(null, key ? [i, arr[i]] : [arr[i]]);
-		},
-		
-		// strict type checker to simplify function overloading (see github.com/gillescochez/is.js for a full version independant)
-		is = (function(arr, obj) {
-			
-			each(arr, function(type) {
-				(function(type) {
-					obj[type] = function(it) {
-						return it.constructor === window[type];
-					};
-				})(type);
-			});
-			
-			return obj;
-			
-		})(['Array','String','Object','Function'], {}),
-		
-		// load a js file and execute a callback when it is loaded 
-		// inspired from http://www.nczonline.net/blog/2009/07/28/the-best-way-to-load-external-javascript/
-		load = function(url, callback){
+    // private
+    var 
+    
+        // settings
+        settings = {
+            path: './modules/',
+            order: true
+        },
+        
+        // stack to process
+        stack = {},
+        
+        // track loaded and loading resources
+        loaded = {},
+        loading = {},
+        
+        // store objects created from "module" declaration
+        objects = {},
+        
+        // settings setter/getter
+        config = function(name, value) {
+            if (!settings[name]) throw name + ' is not a configuration parameter';
+            if (value) settings[name] = value;
+            else return settings[name];
+        },
+        
+        // helper function to iterate over arrays (if key true return the key as first argument and balue second)
+        each = function(arr, fn, key) {
+            var len = arr.length, i = 0;
+            for (; i < len; i++) fn.apply(null, key ? [i, arr[i]] : [arr[i]]);
+        },
+        
+        // strict type checker to simplify function overloading (see github.com/gillescochez/is.js for a full version independant)
+        is = (function(arr, obj) {
+                
+            each(arr, function(type) {
+                (function(type) {
+                    obj[type] = function(it) {
+                        return it.constructor === window[type];
+                    };
+                })(type);
+            });
+            
+            return obj;
+                
+        })(['Array','String','Object','Function'], {}),
+        
+        // load a js file and execute a callback when it is loaded 
+        // inspired from http://www.nczonline.net/blog/2009/07/28/the-best-way-to-load-external-javascript/
+        load = function(url, callback){
 
-			var script = doc.createElement('script');
+            var script = doc.createElement('script');
 
-			if (script.readyState) {  //IE
-				script.onreadystatechange = function() {
-					if (/loaded|complete/.test(script.readyState)) {
-						script.onreadystatechange = null;
-						callback();
-					};
-				};
-			}
-			else script.onload = callback;
+            if (script.readyState) {  //IE
+                script.onreadystatechange = function() {
+                    if (/loaded|complete/.test(script.readyState)) {
+                        script.onreadystatechange = null;
+                        callback();
+                    };
+                };
+            }
+            else script.onload = callback;
 
-			script.src = url;
-			doc.getElementsByTagName('head')[0].appendChild(script);
-		},
-		
-		// convert resources array into a hash string (used by the stack)
-		getHash = function(resources) {
-			return resources.join('');
-		},
-		
-		// convert an array of resources into URLs pointing to the resource file
-		convert = function(resources) {
-			var arr = [];
-			each(resources, function(resource) {
-				if (/^(http\:\/\/|https\:\/\/|\.)/.test(resource)) arr.push(resource);
-				else arr.push(settings.path + resource + '.js');
-			});
-			return arr;
-		},
-		
-		// create a argument array made out of module required in the resources request
-		args = function(resources) {
-			var arr = [];
-			each(resources, function(resource) {
-				if (!/^(http\:\/\/|https\:\/\/|\.)/.test(resource)) arr.push(objects[resource]);
-			});
-			return arr;
-		},
-		
-		// execute a stack of callback for a given request
-		execute = function(hash) {
-			each(stack[hash].callbacks, function(i, callback) {
-				if (settings.order) {
-					if (!stack[hash][i - 1] && callback) {
-						callback.apply(this, args(stack[hash].resources));
-						stack[hash].callbacks.splice(i, 1);
-						execute(hash);
-					};
-				}
-				else callback.apply(this, args(stack[hash].resources));
-			}, true);
-		},
-		
-		// executed when a resource load, it checks for loading state of resources 
-		// requested and call the execute function if criteria are met
-		done = function(resources) {
+            script.src = url;
+            doc.getElementsByTagName('head')[0].appendChild(script);
+        },
+        
+        // convert resources array into a hash string (used by the stack)
+        getHash = function(resources) {
+            return resources.join('');
+        },
+        
+        // convert an array of resources into URLs pointing to the resource file
+        convert = function(resources) {
+            var arr = [];
+            each(resources, function(resource) {
+                if (/^(http\:\/\/|https\:\/\/|\.)/.test(resource)) arr.push(resource);
+                else arr.push(settings.path + resource + '.js');
+            });
+            return arr;
+        },
+        
+        // create a argument array made out of module required in the resources request
+        args = function(resources) {
+            var arr = [];
+            each(resources, function(resource) {
+                if (!/^(http\:\/\/|https\:\/\/|\.)/.test(resource)) arr.push(objects[resource]);
+            });
+            return arr;
+        },
+        
+        // execute a stack of callback for a given request
+        execute = function(hash) {
+            each(stack[hash].callbacks, function(i, callback) {
+                if (settings.order) {
+                    if (!stack[hash][i - 1] && callback) {
+                        callback.apply(this, args(stack[hash].resources));
+                        stack[hash].callbacks.splice(i, 1);
+                        execute(hash);
+                    };
+                }
+                else callback.apply(this, args(stack[hash].resources));
+            }, true);
+        },
+        
+        // executed when a resource load, it checks for loading state of resources 
+        // requested and call the execute function if criteria are met
+        done = function(resources) {
 
-			var good = true;
-	
-			each(convert(resources), function(resource) {
-				if (loaded[resource] !== true) good = false;
-			});
-			
-			if (good) execute(getHash(resources));
-		},
-		
-		// process a resources request by loading them up
-		process = function(files, resources) {
-			each(files, function(resource) {
-				if (loaded[resource]) done(resources);
-				else if (!loading[resource] && !loaded[resource]) {
-					loading[resource] = true;
-					load(resource, function() {
-						loaded[resource] = true;
-						loading[resource] = false;
-						done(resources);
-					});
-				};
-			});
-		},
-		
-		// handle a new resources request
-		request = function(resources, callback) {
-		
-			var hash = getHash(resources);
-			
-			// add the callback to the stack
-			if (!stack[hash]) {
-				stack[hash] = {
-					callbacks: [],
-					resources: resources
-				};
-			}
+            var good = true;
 
-			stack[hash].callbacks.push(callback);
-			process(convert(resources), resources);
-		},
-		
-		// used to create "modules"
-		declare = function(name, object) {
-			if (!objects[name]) {
-				objects[name] = object;
-				if (!objects[name].name) objects[name].name = name;
-			} else throw name + ' already exists!';
-		};
-	
-	// public
-	return function() {
-	
-		// 0 argument :(
-		if (arguments.length === 0) {
-			return {
-				settings: settings,
-				objects: objects
-			};
-		};
-		
-		// 1 argument :|
-		if (arguments.length === 1) {
-		
-			// return an object (object must be loaded) var mod = req('module');
-			if (is.String(arguments[0])) {
-				if (objects[arguments[0]]) return objects[arguments[0]];
-				else throw arguments[0] + ' does not exists!';
-			};
-			
-			// set multiple settings in one go req({});
-			if (is.Object(arguments[0])) {
-				for (var name in arguments[0]) config(name, arguments[0][name]);
-			};
-		};
-		
-		// 2 arguments :)
-		if (arguments.length === 2) {
+            each(convert(resources), function(resource) {
+                if (loaded[resource] !== true) good = false;
+            });
+            
+            if (good) execute(getHash(resources));
+        },
+        
+        // process a resources request by loading them up
+        process = function(files, resources) {
+            each(files, function(resource) {
+                if (loaded[resource]) done(resources);
+                else if (!loading[resource] && !loaded[resource]) {
+                    loading[resource] = true;
+                    load(resource, function() {
+                        loaded[resource] = true;
+                        loading[resource] = false;
+                        done(resources);
+                    });
+                };
+            });
+        },
+        
+        // handle a new resources request
+        request = function(resources, callback) {
+        
+            var hash = getHash(resources);
+            
+            // add the callback to the stack
+            if (!stack[hash]) {
+                stack[hash] = {
+                    callbacks: [],
+                    resources: resources
+                };
+            };
 
-			// request req([], function(){});
-			if (is.Array(arguments[0]) && is.Function(arguments[1])) request(arguments[0], arguments[1]);
-			
-			// declaring a module req('module', {} || function(){});
-			if (is.String(arguments[0]) && (is.Object(arguments[1]) || is.Function(arguments[1]))) {
-				declare(arguments[0], arguments[1]);
-			};
+            stack[hash].callbacks.push(callback);
+            process(convert(resources), resources);
+        },
+        
+        // used to create "modules"
+        declare = function(name, object) {
+            if (!objects[name]) {
+                objects[name] = object;
+                if (!objects[name].name) objects[name].name = name;
+            } else throw name + ' already exists!';
+        };
 
-			// configuration parameters setter req('path', './modules/'); and getter req('get','path');
-			if (is.String(arguments[0]) && is.String(arguments[1])) {
-				if (arguments[0] === 'get') return config(arguments[1]);
-				else config(arguments[0], arguments[1]);
-			};
-		};
-		
-		// chainability
-		return req;
-	};
+    // public
+    return function() {
+
+        var args = arguments;
+
+        // 0 argument :(
+        if (args.length === 0) {
+            return {
+                settings: settings,
+                objects: objects
+            };
+        };
+        
+        // 1 argument :|
+        if (args.length === 1) {
+        
+            // return an object (object must be loaded) var mod = req('module');
+            if (is.String(args[0])) {
+                if (objects[args[0]]) return objects[args[0]];
+                else throw args[0] + ' does not exists!';
+            };
+            
+            // set multiple settings in one go req({});
+            if (is.Object(arguments[0])) {
+                for (var name in args[0]) config(name, args[0][name]);
+            };
+        };
+        
+        // 2 arguments :)
+        if (args.length === 2) {
+
+            // request req([], function(){});
+            if (is.Array(args[0]) && is.Function(args[1])) request(args[0], args[1]);
+            
+            // declaring a module req('module', {} || function(){});
+            if (is.String(args[0]) && (is.Object(args[1]) || is.Function(args[1]))) {
+                declare(args[0], args[1]);
+            };
+
+            // configuration parameters setter req('path', './modules/'); and getter req('get','path');
+            if (is.String(args[0]) && is.String(args[1])) {
+                if (args[0] === 'get') return config(args[1]);
+                else config(args[0], args[1]);
+            };
+        };
+        
+        // chainability
+        return req;
+    };
 
 })(document);
--- tests/tests.js
+++ tests/tests.js
@@ -1,85 +1,85 @@
 test('Structure', function() {
-	expect(2);
-	ok(req === window.req, 'req is globally defined');
-	ok(req.constructor == Function, 'req is a function');
+    expect(2);
+    ok(req === window.req, 'req is globally defined');
+    ok(req.constructor == Function, 'req is a function');
 });
 
 test('Configuration', function() {
 
-	// expected default object
-	var expected = {
-		settings: {
-			path: './modules/',
-			order: true
-		},
-		objects: {}
-	};
-	
-	expect(6);
-	
-	// testing default configuration
-	deepEqual(req(), expected, 'default object available');
-	deepEqual(req().settings, expected.settings, 'default settings object');
-	deepEqual(req().objects, expected.objects, 'default objects object');
-	
-	// configuration parameter getter
-	equal(req('get','path'), expected.settings.path, 'configuration parameter getter');
-	
-	// configuration parameter setter
-	var pathExpected = './';
-	req('path', pathExpected);
-	equal(req('get','path'), pathExpected, 'configuration parameter setter');
-	req('path', expected.settings.path);
-	
-	// configuration changes via object (to change more than one parameter when we actually have more than one lol)
-	req({path:pathExpected});
-	equal(req('get','path'), pathExpected, 'configuration changes via object');
-	req('path', expected.settings.path);
+    // expected default object
+    var expected = {
+        settings: {
+            path: './modules/',
+            order: true
+        },
+        objects: {}
+    };
+    
+    expect(6);
+    
+    // testing default configuration
+    deepEqual(req(), expected, 'default object available');
+    deepEqual(req().settings, expected.settings, 'default settings object');
+    deepEqual(req().objects, expected.objects, 'default objects object');
+    
+    // configuration parameter getter
+    equal(req('get','path'), expected.settings.path, 'configuration parameter getter');
+    
+    // configuration parameter setter
+    var pathExpected = './';
+    req('path', pathExpected);
+    equal(req('get','path'), pathExpected, 'configuration parameter setter');
+    req('path', expected.settings.path);
+    
+    // configuration changes via object (to change more than one parameter when we actually have more than one lol)
+    req({path:pathExpected});
+    equal(req('get','path'), pathExpected, 'configuration changes via object');
+    req('path', expected.settings.path);
 	
 	
 });
 
 setTimeout(function() {
 
-	req(['dummyObject','./jquery.js', 'dummyClass'], function(obj, clas) {
-		test('Loading resources', function() {
-		
-			expect(7);
-		
-			ok(window.jQuery, 'jQuery in the global scope');
-		
-			ok(obj, 'dummyObject availability');
-			ok(clas, 'dummyClass availability');
-			
-			equal(obj.name, 'dummyObject', 'object name');
-			equal(clas.name, 'Class', 'class name');
-			
-			ok(obj.constructor == Object, 'dummyObject constructor Object');
-			ok(clas.constructor == Function, 'dummyClass constructor Function');
-		});
-	});
+    req(['dummyObject','./jquery.js', 'dummyClass'], function(obj, clas) {
+        test('Loading resources', function() {
+        
+            expect(7);
+    
+            ok(window.jQuery, 'jQuery in the global scope');
+    
+            ok(obj, 'dummyObject availability');
+            ok(clas, 'dummyClass availability');
+            
+            equal(obj.name, 'dummyObject', 'object name');
+            equal(clas.name, 'Class', 'class name');
+            
+            ok(obj.constructor == Object, 'dummyObject constructor Object');
+            ok(clas.constructor == Function, 'dummyClass constructor Function');
+        });
+    });
 }, 250);
 
 setTimeout(function() {
 
-	var foo = false;
+    var foo = false;
 
-	// order of callback
-	req(['dummyObject','dummyClass'], function(obj, clas) {
-	
-		test('Callbacks ordering part 1', function() {
-			expect(1);
-			equal(foo, false, 'first ordered callback - foo should be false');
-		});
-		
-		foo = true;
-	});
-	
-	req(['dummyObject','dummyClass'], function(obj, clas) {
-		test('Callbacks ordering part 2', function() {
-			expect(1);
-			equal(foo, true, 'second ordered callback - foo should be true as set in first callback');
-		});
-	});
+    // order of callback
+    req(['dummyObject','dummyClass'], function(obj, clas) {
+    
+        test('Callbacks ordering part 1', function() {
+            expect(1);
+            equal(foo, false, 'first ordered callback - foo should be false');
+        });
+        
+        foo = true;
+    });
+    
+    req(['dummyObject','dummyClass'], function(obj, clas) {
+        test('Callbacks ordering part 2', function() {
+            expect(1);
+            equal(foo, true, 'second ordered callback - foo should be true as set in first callback');
+        });
+    });
 
 }, 500);
